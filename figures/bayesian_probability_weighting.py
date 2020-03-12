62344bcbb48afa7ebd323e63d1b30581e7991791
Gustavo Landfried <gustavolandfried@gmail.com>
2020-03-12 09:03:13 -0300

    Add first experimental draft

--- figures/bayesian_probability_weighting.py
+++ figures/bayesian_probability_weighting.py
@@ -1,10 +1,27 @@
 import numpy as np
-import scipy
+import scipy.stats as s
 import matplotlib.pyplot as plt
 
-a=2;b=3
-x = np.linspace(0, 1,101)
-y = scipy.stats.beta.pdf(x, a, b)
-plt.plot(x, y, 'r-', lw=5, alpha=0.6, label='beta pdf')
+a=100;b=100
+grilla = np.linspace(0, 1,10001)
+y = s.beta.pdf(grilla, a, b)
+plt.plot(grilla, y, 'r-', lw=5, alpha=0.6, label='beta pdf')
+grilla[np.argmax(y)]
 
-MAP = x[np.argmax(y)]
+s.beta.stats(a, b)
+
+total = 100
+maximum_a_posteriori = []
+x = np.linspace(0, 1,total + 1)
+mean = []
+for i in x:#i=0.05
+    a = 0.00001; b = 0.00001
+    caras = int(total*i)
+    a += caras; b += total-caras
+    #mean.append(float(s.beta.stats(a, b)[0]) )
+    y = s.beta.pdf(grilla, a, b)
+    maximum_a_posteriori.append(grilla[np.argmax(y)])
+
+#plt.plot(x,mean)
+plt.plot(x,maximum_a_posteriori)
+plt.plot(x,x)
